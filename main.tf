8ff09dfadf2a7019ba253a94cde28fa1c9b1eb28
Bhaskar010216 <braj1044@gmail.com>
2025-08-16 13:15:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,61 @@
+resource "azurerm_resource_group" "sunny" {
+  name     = var.rg
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "sunny" {
+  name                = var.vn
+  address_space       = var.as
+  location            = azurerm_resource_group.sunny.location
+  resource_group_name = azurerm_resource_group.sunny.name
+}
+
+resource "azurerm_subnet" "sunny" {
+  name                 = var.sn
+  resource_group_name  = azurerm_resource_group.sunny.name
+  virtual_network_name = azurerm_virtual_network.sunny.name
+  address_prefixes     = var.address_prefixes
+}
+
+resource "azurerm_public_ip" "sunny" {
+  name                = var.ip
+  resource_group_name = azurerm_resource_group.sunny.name
+  location            = azurerm_resource_group.sunny.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "sunny" {
+  name                = var.nic
+  location            = azurerm_resource_group.sunny.location
+  resource_group_name = azurerm_resource_group.sunny.name
+
+ip_configuration {
+  name                          = "internal"
+  subnet_id                     = azurerm_subnet.sunny.id
+  public_ip_address_id          = azurerm_public_ip.sunny.id
+  private_ip_address_allocation = "Dynamic"
+}
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                  = var.vmname
+  resource_group_name   = azurerm_resource_group.sunny.name
+  location              = azurerm_resource_group.sunny.location
+  size                  = var.size
+  admin_username        = var.username
+  admin_password        = var.password
+  network_interface_ids = [azurerm_network_interface.sunny.id]
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
